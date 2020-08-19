5ac1dcd73eb8e9594e9391db3cf9657b1c36ddc7
Seth Mariscotes <seth.mariscotes@yahoo.com>
2020-08-19 16:42:39 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -9,4 +9,5 @@ end
  it 'defaults to Ruby Programmer when no name is passed in' do
    expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
    say_hello("Ruby Programmer")
-   
+ end
+ 
